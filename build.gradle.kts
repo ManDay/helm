e52d39d6cc4a5585bcfc5a44f75e20414d69760a
Cedric Sodhi <ManDay@users.noreply.github.com>
2022-02-04 21:57:54 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,11 +11,11 @@ repositories {
 }
 
 android {
-    compileSdkVersion(30)
+    compileSdkVersion(31)
     defaultConfig {
         applicationId = "com.github.manday.helm"
         minSdkVersion(16)
-        targetSdkVersion(30)
+        targetSdkVersion(31)
         versionCode = 1
         versionName = "1.0"
     }
