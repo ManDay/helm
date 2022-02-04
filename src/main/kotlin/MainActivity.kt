ca73b6dafcfa007874709293b40119a2058d6f91
Cedric Sodhi <manday@openmail.cc>
2022-02-04 21:14:12 +0200

    Again

---
+++ src/main/kotlin/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.github.manday.helm;
+
+import android.os.Bundle;
+import android.app.Activity;
+import	android.content.Intent;
+import android.widget.TextView;
+
+const val OPEN_DOCUMENT_TREE: Int = 1;
+
+class MainActivity : Activity() {
+ override fun onActivityResult( requestCode:Int, resultCode:Int, resultData:Intent? ) {
+  if (requestCode==OPEN_DOCUMENT_TREE && resultCode==Activity.RESULT_OK ) {
+  }
+ }
+
+ override fun onCreate(savedInstanceState:Bundle?) {
+  super.onCreate(savedInstanceState);
+  val output = TextView(this);
+  this.setContentView(output);
+  output.setText("This is a te[xs]t");
+ }
+}
