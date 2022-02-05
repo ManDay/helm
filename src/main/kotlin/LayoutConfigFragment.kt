2792befc43222e8fbfb2a6535b30efccb49b8a7d
Cedric Sodhi <manday@openmail.cc>
2022-02-05 11:38:21 +0200

    Commit

---
+++ src/main/kotlin/LayoutConfigFragment.kt
@@ -0,0 +1,5 @@
+package com.github.manday.helm;
+
+import androidx.fragment.app.Fragment;
+
+class LayoutConfigFragment : Fragment( R.layout.layout_config_fragment )
--- src/main/kotlin/PreferencesActivity.kt
+++ src/main/kotlin/PreferencesActivity.kt
@@ -3,7 +3,7 @@ package com.github.manday.helm;
 import android.os.Bundle;
 import androidx.fragment.app.FragmentActivity;
 
-class Preferences : FragmentActivity() {
+class PreferencesActivity : FragmentActivity() {
  override fun onCreate( savedInstanceState : Bundle? ) {
   super.onCreate( savedInstanceState );
   setContentView( R.layout.preferences_activity )
