3b5bf0d98090edc49d91d606e17a1e2562f70bd6
Cedric Sodhi <manday@openmail.cc>
2022-02-19 18:37:03 +0200

    Commit

--- src/main/kotlin/PreferencesActivity.kt
+++ src/main/kotlin/PreferencesActivity.kt
@@ -1,7 +1,7 @@
 package com.github.manday.helm;
 
 import android.os.Bundle;
-import androidx.activity-compose.ComponentActivity;
+import androidx.activity.ComponentActivity;
 
 class PreferencesActivity : ComponentActivity() {
  override fun onCreate( savedInstanceState : Bundle? ) {
