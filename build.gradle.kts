e93c3928785a04d9ee641a7d394b6d494e3d8ade
Cedric Sodhi <manday@openmail.cc>
2022-03-02 16:16:42 +0200

    Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,4 +31,5 @@ dependencies {
 /*    implementation("androidx.compose.ui:ui:1.1.0") */
     implementation("androidx.activity:activity-compose:1.4.0")
     implementation("androidx.compose.material:material:1.1.0")
+    implementation("com.google.android.material:material:1.5.0")
 }
