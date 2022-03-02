f54fd6800d7aeb2a4f9ec1d3e01aea77ee606b0a
Cedric Sodhi <manday@openmail.cc>
2022-03-02 17:34:23 +0200

    Commit

--- src/main/kotlin/PreferencesActivity.kt
+++ src/main/kotlin/PreferencesActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import androidx.activity.ComponentActivity;
 import androidx.activity.compose.setContent;
 
+import androidx.compose.runtime.Composable;
+
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
