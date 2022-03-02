e25a0818fef7299f041b84696f0dcabfeea321d1
Cedric Sodhi <manday@openmail.cc>
2022-03-02 15:39:30 +0200

    Commit

--- src/main/kotlin/PreferencesActivity.kt
+++ src/main/kotlin/PreferencesActivity.kt
@@ -9,7 +9,58 @@ class PreferencesActivity : ComponentActivity() {
  override fun onCreate( savedInstanceState : Bundle? ) {
   super.onCreate( savedInstanceState );
   setContent {
-   Text( "Hello Helm!" )
+   PreferencesComposable( )
   }
  }
+}
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.ExtendedFloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.rememberCoroutineScope
+
+val scaffoldState = rememberScaffoldState()
+val scope = rememberCoroutineScope()
+
+@Composable
+fun PreferencesComposable( ) {
+  Scaffold(
+    scaffoldState = scaffoldState,
+    drawerContent = { Text("Drawer content") },
+    topBar = {
+      TopAppBar(
+        title = { Text("Simple Scaffold Screen") },
+        navigationIcon = {
+          IconButton(
+            onClick = { scope.launch { scaffoldState.drawerState.open() } }
+          ) {
+            Icon(Icons.Filled.Menu, contentDescription = "Localized description")
+          }
+        }
+      )
+    },
+    floatingActionButtonPosition = FabPosition.End,
+    floatingActionButton = {
+      ExtendedFloatingActionButton(
+        text = { Text("Inc") },
+        onClick = { /* fab click handler */ }
+      )
+    },
+    content = {
+      innerPadding -> LazyColumn(contentPadding = innerPadding) {
+        items(count = 100) {
+          Box( Modifier.fillMaxWidth().height(50.dp).background(colors[it % colors.size]) )
+        }
+      }
+    }
+  )
 }
