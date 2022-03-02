308cef60ea761bd3b8a31e632e7cfb6bb9986950
Cedric Sodhi <manday@openmail.cc>
2022-03-02 18:55:51 +0200

    Commit

--- src/main/kotlin/PreferencesActivity.kt
+++ src/main/kotlin/PreferencesActivity.kt
@@ -22,6 +22,7 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
