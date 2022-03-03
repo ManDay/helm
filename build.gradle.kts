4719e641ff860d989cd5c159482b666dfa343f52
Cedric Sodhi <manday@openmail.cc>
2022-03-03 10:51:41 +0200

    Commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,5 +34,5 @@ dependencies {
     implementation("androidx.compose.material:material:1.1.0")
     implementation("androidx.compose.material:material-icons-core:1.1.0") 
     implementation("androidx.compose.runtime:runtime:1.1.0")
-/*    implementation("com.google.android.material:material:1.5.0") */
+    implementation("com.google.android.material:material:1.5.0")
 }
