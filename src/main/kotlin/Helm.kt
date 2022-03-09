1547793f93405c3a184503d42100235185d82fc0
Cedric Sodhi <manday@openmail.cc>
2022-03-09 20:34:44 +0200

    Commit

--- src/main/kotlin/Helm.kt
+++ src/main/kotlin/Helm.kt
@@ -3,11 +3,13 @@ package com.github.manday.helm;
 import android.app.Application;
 import android.os.Bundle;
 import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.snapshots.SnapshotStateList
 
 class Helm: Application() {
- val layoutRepository = mutableStateListOf<Layout>( );
+ var layoutRepository : SnapshotStateList<Layout>?
  override fun onCreate( ) {
   super.onCreate( );
+  this.layoutRepository = mutableStateListOf<Layout>( );
  }
 }
 
