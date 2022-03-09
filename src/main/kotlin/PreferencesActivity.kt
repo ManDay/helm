b17a1758103df593665782ea6592e296b92486b6
Cedric Sodhi <manday@openmail.cc>
2022-03-09 17:07:01 +0200

    Commit

--- src/main/kotlin/PreferencesActivity.kt
+++ src/main/kotlin/PreferencesActivity.kt
@@ -1,11 +1,13 @@
 package com.github.manday.helm;
 
+import kotlinx.coroutines.launch
+
 import android.os.Bundle;
 
 import androidx.activity.ComponentActivity;
 import androidx.activity.compose.setContent;
-import androidx.compose.runtime.Composable;
 
+import androidx.compose.runtime.Composable;
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
