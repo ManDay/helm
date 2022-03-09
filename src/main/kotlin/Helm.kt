bddd955eb2fdd5232e925a072d7b7916f89497f3
Cedric Sodhi <manday@openmail.cc>
2022-03-09 16:36:58 +0200

    Commit

--- src/main/kotlin/Helm.kt
+++ src/main/kotlin/Helm.kt
@@ -4,10 +4,10 @@ import android.app.Application;
 import android.os.Bundle;
 
 class Helm: Application() {
+ val layoutRepository : LayoutRepository;
  override fun onCreate( ) {
   super.onCreate( );
   this.layoutRepository = LayoutRepository( );
  }
- error
 }
 
